2905bdcb5f9a01f419188eb959e87b1583548d16
Alessio Palmero Aprosio <alessio@apnetwork.it>
2016-09-06 16:02:11 +0200

    Properties

--- utils-core/src/main/java/eu/fbk/utils/core/PropertiesUtils.java
+++ utils-core/src/main/java/eu/fbk/utils/core/PropertiesUtils.java
@@ -1,6 +1,8 @@
 package eu.fbk.utils.core;
 
 import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by alessio on 05/08/16.
@@ -10,6 +12,21 @@ public class PropertiesUtils {
 
     private static String[] booleanTrue = new String[] { "yes", "1", "y", "true" };
     private static String[] booleanFalse = new String[] { "no", "0", "n", "false" };
+
+    public static Properties dotConvertedProperties(Properties originalProperties, String prefix) {
+        Properties ret = new Properties();
+
+        for (Map.Entry<Object, Object> entry : originalProperties.entrySet()) {
+            if (entry.getKey() instanceof String) {
+                if (((String) entry.getKey()).startsWith(prefix)) {
+                    String newKey = ((String) entry.getKey()).substring(prefix.length() + 1);
+                    ret.put(newKey, entry.getValue());
+                }
+            }
+        }
+
+        return ret;
+    }
 
     public static Integer getInteger(@Nullable String value, int defaultValue) {
         try {
@@ -31,12 +48,14 @@ public class PropertiesUtils {
                     return false;
                 }
             }
+            try {
+                return Boolean.parseBoolean(value);
+            } catch (Exception e) {
+                return defaultValue;
+            }
         }
-        try {
-            return Boolean.parseBoolean(value);
-        } catch (Exception e) {
-            return defaultValue;
-        }
+
+        return defaultValue;
     }
 
     public static Double getDouble(@Nullable String value, double defaultValue) {
